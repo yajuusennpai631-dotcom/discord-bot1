135f0e3082489066e463c87e64e01dadb4e9a2b6
Your Name <you@example.com>
2026-06-11 00:42:00 +0900

    first bot

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(
+command_prefix="!",
+intents=intents
+)
+
+@bot.event
+async def on_ready():
+print(f"{bot.user} が起動しました！")
+
+bot.run(TOKEN)
