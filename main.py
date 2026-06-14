278e694c7b4a532adadf209e3bfc95ba7470fc6c
MKMK <yajuusennpai631@gmail.com>
2026-06-14 12:46:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-﻿import os
+import os
 import discord
 from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD_ID = 1488795327069945970
 
 intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
 
 bot = commands.Bot(
     command_prefix="!",
     intents=intents
 )
 
+guild = discord.Object(id=GUILD_ID)
+
 @bot.event
 async def on_ready():
+    synced = await bot.tree.sync(guild=guild)
+    print(f"{len(synced)}個のコマンドを同期しました")
     print(f"{bot.user} is now online!")
+
+@bot.tree.command(
+    name="hello",
+    description="あいさつするコマンド",
+    guild=guild
+)
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message("おはよう")
 
 bot.run(TOKEN)
